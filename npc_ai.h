20fa417dc4df5e1b60e497024d529c38a7e9e5a6
Sabareesan-007 <iamsabareesan@gmail.com>
2024-07-29 18:06:34 +0530

    npc_ai.h
    # Header for NPC AI enhancements

---
+++ npc_ai.h
@@ -0,0 +1,12 @@
+#ifndef NPC_AI_H
+#define NPC_AI_H
+
+class EnhancedNPC {
+public:
+    void UpdateBehavior();
+private:
+    void ExecuteAction(const std::string &action);
+    // Other private methods
+};
+
+#endif // NPC_AI_H
