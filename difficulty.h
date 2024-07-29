5130d8e827bdd6c94db5252eac7002638feb67ee
Sabareesan-007 <iamsabareesan@gmail.com>
2024-07-29 18:08:52 +0530

    difficulty.h
    # Header for Dynamic Difficulty Adjustment

---
+++ difficulty.h
@@ -0,0 +1,13 @@
+#ifndef DIFFICULTY_H
+#define DIFFICULTY_H
+
+class DifficultyManager {
+public:
+    void UpdateDifficulty();
+private:
+    void SetDifficulty(int level);
+    // Other private methods
+};
+
+#endif // DIFFICULTY_H
+
